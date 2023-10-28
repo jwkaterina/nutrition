5f8e9e38eecbebdc5169018eb4f4127a51cc88dd
jwkaterina <jwkaterina@gmail.com>
2023-10-28 19:01:40 +0200

    style home

--- src/app/components/page-grid.tsx
+++ src/app/components/page-grid.tsx
@@ -4,11 +4,14 @@ import styels from './page-grid.module.css'
 import Card from './card'
 
 const PageGrid = ( { children }) => {
+
+    const mediaQuery = window.matchMedia('(max-width: 600px)');
+
     return (
         <div className={styels.container}>
             <div className={styels.grid}>
                 {children}
-                <Card title={null} text={null} type={'add'} />
+                {!mediaQuery.matches && <Card title={null} text={null} type={'add'} />}
             </div>
 
         </div>
