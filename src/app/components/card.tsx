3b46492e066db47c02b9b724fb8dfe87cdc4d4c4
jwkaterina <jwkaterina@gmail.com>
2023-10-29 18:04:48 +0100

    fix open card animation for mobile

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -15,15 +15,31 @@ const Card = ({ title, text, index }: CardProps): JSX.Element => {
 
     const cardRef = useRef<HTMLDivElement>(null);
 
-    const column: number = index % 4 === 0 ? 4 : index % 4;
-    const row: number = Math.ceil(index / 4);
-    const translateX: number = (1 - column) * 25;
-    const translateY: number = (1 - row) * 150;
-    const top: number = - row;
+    const mediaQuery: MediaQueryList = window.matchMedia('(max-width: 600px)');
+
+    const cardHeight: number = cardRef.current ? cardRef.current.clientHeight : 0;
+    const gridGap: number = 1;
+    const headerHeight: number = 80;
+    const footerHeight: number = 80;
+
+    let column: number, row: number, translateX: number, height: number;
+    if(mediaQuery.matches) {
+        column = index % 2 === 0 ? 2 : index % 2;
+        row = Math.ceil(index / 2);
+        translateX = (1 - column) * 50;
+        height = window.innerHeight - headerHeight - footerHeight;
+    } else {
+        column = index % 4 === 0 ? 4 : index % 4;
+        row = Math.ceil(index / 4);
+        translateX = (1 - column) * 25;
+        height = window.innerHeight - headerHeight;
+    }
+    const translateY: number = (1 - row) * cardHeight;
+    const top: number = - gridGap * row;
 
     const keyframes: Keyframe[] = [
-        { top: 0, left: 0, width: '100%', height: '150px', zIndex: 1 },
-        { top: `${top}rem`, left: '-1rem', width: '100vw', height: '100vh', zIndex: 1, transform: `translate(${translateX}%, ${translateY}px)`},
+        { top: 0, left: 0, width: '100%', height: `${cardHeight}px`, zIndex: 1 },
+        { top: `${top}rem`, left: `-${gridGap}rem`, width: '100vw', height: `${height}px`, zIndex: 1, transform: `translate(${translateX}%, ${translateY}px)`},
     ];
 
     const options: KeyframeAnimationOptions  = {
@@ -34,6 +50,7 @@ const Card = ({ title, text, index }: CardProps): JSX.Element => {
 
     if(cardRef.current && openCard) {
         cardRef.current.animate(keyframes, options);
+        document.body.style.overflow = 'hidden';
     }
 
     return (
