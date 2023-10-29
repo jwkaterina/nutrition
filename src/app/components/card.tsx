ebabed96cd8bfef63eaffea834f32d2dcd3b9cee
jwkaterina <jwkaterina@gmail.com>
2023-10-29 16:11:49 +0100

    expand card on click

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,14 +1,29 @@
 'use client'
 
 import styles from './card.module.css'
+import { useState } from 'react'
 
 interface CardProps {
     title: string | null,
-    text: string | null,
-    type?: string
+    text: string | number | null,
+    type: string,
+    index?: number
 }
 
-const Card = ({ title, text, type }: CardProps): JSX.Element => {
+const Card = ({ title, text, type, index }: CardProps): JSX.Element => {
+
+    const [openCard, setOpenCard] = useState<boolean>(false);
+
+    const calculateCardClassName = (): string => {
+        let className: string = styles.card;
+        if(openCard && index == 0) className += ' ' + styles.expand0;
+        if(openCard && index == 1) className += ' ' + styles.expand1;
+        if(openCard && index == 2) className += ' ' + styles.expand2;
+        if(openCard && index == 3) className += ' ' + styles.expand3;
+        if(openCard && index == 4) className += ' ' + styles.expand4;
+        return className;
+    }
+
     if(type === 'add') return (
         <div className={styles.card}>
             <div className={styles.add}>
@@ -18,7 +33,10 @@ const Card = ({ title, text, type }: CardProps): JSX.Element => {
     )
 
     return (
-        <div className={styles.card}>
+        <div className={calculateCardClassName()} onClick={() => {
+            setOpenCard(true);
+            console.log(index);
+        }}>
             <h2>{title}</h2>
             <p>{text}</p>
         </div>
