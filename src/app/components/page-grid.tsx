fb08fbfdcb85ffbd7e539eb99ff77fb504b8cbc9
jwkaterina <jwkaterina@gmail.com>
2023-10-31 19:48:43 +0100

    add links to add buttons

--- src/app/components/page-grid.tsx
+++ src/app/components/page-grid.tsx
@@ -1,29 +1,33 @@
 'use client'
 
+import Link from 'next/link'
 import styles from './page-grid.module.css'
 import cardStyles from './card.module.css'
 
 interface PageGridProps {
+    search: string,
     children: JSX.Element[]
 }
 
-const PageGrid = ( { children }: PageGridProps): JSX.Element => {
+const PageGrid = ( { search, children }: PageGridProps): JSX.Element => {
 
     const mediaQuery = window.matchMedia('(max-width: 600px)'); 
 
     const Button = () => {
         return (
             <div className={cardStyles.card}>
-                <div className={styles.add}>
+                <Link href={`/${search}`} className={styles.link}>
                     <div className={styles.add__icon}>+</div>
-                </div>
+                </Link> 
             </div>
         )
     }
 
     const MobileButton = () => {
         return (
-            <button className={styles.button}>+</button>
+            <button className={styles.button}>
+                <Link href={`/${search}`} className={styles.mobile_link}>+</Link>
+            </button>
         )
     }
 
