3ff89535c50d111f815c8498f722c713160a52d1
jwkaterina <jwkaterina@gmail.com>
2023-11-13 15:11:31 +0100

    clean code

--- src/app/components/nav-bar/nav-bar.tsx
+++ src/app/components/nav-bar/nav-bar.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-// import Link from "next/link"
 import styles from "./nav-bar.module.css"
-// import { usePathname} from 'next/navigation';
 
 interface NavBarProps {
     color: string,
@@ -11,19 +9,8 @@ interface NavBarProps {
 
 const NavBar = ({ color, children }: NavBarProps): JSX.Element => {
 
-    // const pathname = usePathname();
-
-
     return (
         <nav className={styles.container} style={{background: color}}>
-            {/* <Link href="/" className={pathname == "/" ? `${styles.active} ${styles.link}` : styles.link}>Home
-            </Link> */}
-            {/* <Link href="/food" className={pathname == "/food" ? `${styles.active} ${styles.link}` : styles.link}>Food
-            </Link>
-            <Link href="/recipe" className={pathname == "/recipe" ? `${styles.active} ${styles.link}` : styles.link}>Recipe
-            </Link>
-            <Link href="/menu" className={pathname == "/menu" ? `${styles.active} ${styles.link}` : styles.link}>Menu
-            </Link> */}
             {children}
         </nav>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import styles from './page.module.css'
-// import { fetchNutritionAnalysisAPI } from '@/app/services/fetch-data'
 import { useContext, useState } from 'react' 
 import NavBar from '@/app/components/nav-bar/nav-bar';
 import Slider from '@/app/components/slider';
@@ -11,18 +9,6 @@ import OpenCardMenu from '@/app/components/nav-bar/menus/opencard-menu';
 import { CardOpenContext } from './context/card-context';
 
 export default function Home(): JSX.Element {
-
-  // const [data, setData] = useState({calories: 0});
-
-  // useEffect(() => {
-  //   async function fetchData() {
-
-  //     const response = await fetchNutritionAnalysisAPI();
-  //     // console.log(response);
-  //     setData(response);
-  //   }
-  //   fetchData();
-  // }, []);
 
   const [slide, setSlide] = useState<SlideType>(SlideType.FOOD);
   const [blockScrollHandler, setBlockScrollHandler] = useState<boolean>(false);
