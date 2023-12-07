c3cebb420bbf2ad07857519b627ebcceaedf0ef7
jwkaterina <jwkaterina@gmail.com>
2023-12-07 20:22:51 +0100

    animate bars

--- src/app/components/analysis_cards/utils/bar_column.tsx
+++ src/app/components/analysis_cards/utils/bar_column.tsx
@@ -1,5 +1,6 @@
 import styles from '../alanysis_card.module.css'
 import { Nutrient } from '@/app/types/types'
+import { useEffect, useRef } from 'react'
 
 interface BarColumnProps {
     vitamin: Nutrient,
@@ -15,11 +16,30 @@ const BarColumn = ({ vitamin, vitaminPercent, label, color, lightColor }: BarCol
     let percentHeight = 0;
     if(vitaminPercent) percentHeight = vitaminPercent.quantity / 100 * barHeight;
 
+    const barRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const options: KeyframeAnimationOptions  = {
+            duration: 1000,
+            easing: 'ease-in-out',
+            fill: 'forwards'
+        };
+        const keyframes: Keyframe[] = [
+            { height: 0 },
+            { height: `${percentHeight}px` }
+        ];
+
+        if(barRef.current) {
+            barRef.current.animate(keyframes, options);
+        }
+    })
+    
+
     if(!vitamin) return <></>
 
     return <div className={styles.bar_column}>
                 <div className={styles.bar} style={{height: `${barHeight}px`, backgroundColor: lightColor}}>
-                    <div className={styles.percent_bar} style={{height: `${percentHeight}px`, backgroundColor: color}}></div>
+                    <div className={styles.percent_bar} ref={barRef} style={{backgroundColor: color}}></div>
                 </div>                
                 <p>{label}</p>
                 <p>{`${vitamin.quantity} ${vitamin.unit}`}</p>
