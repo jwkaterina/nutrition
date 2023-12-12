ccb337430a75e3ab49c160f085bde99ec68e690d
jwkaterina <jwkaterina@gmail.com>
2023-12-12 14:54:22 +0100

    clean page grid

--- src/app/components/page-grid.tsx
+++ src/app/components/page-grid.tsx
@@ -1,21 +1,12 @@
 'use client'
 
-import { useEffect } from 'react';
 import styles from './page-grid.module.css'
-import { CardOpenContext } from '@/app/context/card-context';
 
 interface PageGridProps {
     children: React.ReactNode
 }
 
 const PageGrid = ( { children }: PageGridProps): JSX.Element => {
-
-    const cardOpen = CardOpenContext;
-
-    useEffect(() => {
-        scrollTo(0, 0);
-        console.log('scrolling to top');
-    }, [cardOpen])
 
     return (
             <div className={styles.grid}>
