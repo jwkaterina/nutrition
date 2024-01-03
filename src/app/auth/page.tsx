e828c4b7bf6595bbe314df5a1253a909f00bf03e
jwkaterina <jwkaterina@gmail.com>
2024-01-03 13:12:26 +0100

    create login form

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,16 +1,39 @@
+'use client';
+
 import styles from './page.module.css';
 import NavBar from '@/app/components/navigation/nav-bar';
 import AuthMenu from '@/app/components/navigation/menus/auth-menu';
+import { useState } from 'react';
 
 const Auth = (): JSX.Element => {
 
     const tertiaryColor = "var(--tertiary-color)";
+
+    const [loginMode, setLoginMode] = useState(true);
+
     return (<>
         <NavBar color={tertiaryColor}>
             <AuthMenu />
         </NavBar>
         <div className={styles.container}>
-            <h1>Auth</h1>
+            <form className={styles.form}>
+                {!loginMode && <div className={styles.form_group}>
+                    <label htmlFor="name">Name</label>
+                    <input id="name" type="text" />
+                </div>}
+                <div className={styles.form_group}>
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="email" />
+                </div>
+                <div className={styles.form_group}>
+                    <label htmlFor="password">Password</label>
+                    <input id="password" type="password" />
+                </div>
+                <div className={styles.form_group}>
+                    <button type="submit">{loginMode ? 'Login' : 'Signup'}</button>
+                </div>
+            </form>
+            <button className={styles.button} onClick={() => setLoginMode(!loginMode)}>{`Switch to ${loginMode ? 'Signup' : 'Login'}`}</button>
         </div>
     </>
     )
