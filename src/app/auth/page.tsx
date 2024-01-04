d3e158ec6caed313da019c2d69fbeb045ace7c05
jwkaterina <jwkaterina@gmail.com>
2024-01-04 13:12:33 +0100

    login and go back

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -7,28 +7,55 @@ import { AuthContext, SetAuthContext } from '@/app/context/auth-context';
 import { useState, useContext } from 'react';
 import LoadingSpinner from '@/app/components/loading/loading-spinner';
 import ErrorModal from '../components/error-modal/error-modal';
+import { useRouter} from 'next/navigation';
+import { SetBlockScrollContext } from '@/app/context/slide-context';
 
 const Auth = (): JSX.Element => {
 
     const tertiaryColor = "var(--tertiary-color)";
 
     const isLoggedIn = useContext(AuthContext);
+    const setBlockScroll = useContext(SetBlockScrollContext);
     const setIsLoggedIn = useContext(SetAuthContext);
     const [loginMode, setLoginMode] = useState(true);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null); 
+
+    const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const form = e.currentTarget;
         const email = form.email.value;
         const password = form.password.value;
+        setIsLoading(true);
         if (loginMode) {
-            console.log(email, password)
+            try {
+                const response = await fetch('http://localhost:5001/auth/login', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    email: email,
+                    password: password
+                  })
+                });
+        
+                const responseData = await response.json();
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                  }
+                  setIsLoading(false);
+                  setIsLoggedIn(true);
+                  router.back();
+            } catch (err) {
+                setIsLoading(false);
+                setError(err.message || 'Something went wrong, please try again.');
+            }
         } else {
             const name = form.name.value;
             try {
-                setIsLoading(true);
                 const response = await fetch('http://localhost:5001/auth/signup', {
                   method: 'POST',
                   headers: {
@@ -45,14 +72,22 @@ const Auth = (): JSX.Element => {
                 if (!response.ok) {
                     throw new Error(responseData.message);
                   }
-                  console.log(responseData);
                   setIsLoading(false);
                   setIsLoggedIn(true);
-              } catch (err) {
+                  router.back();
+            } catch (err) {
                 setIsLoading(false);
                 setError(err.message || 'Something went wrong, please try again.');
-              }
+            }
         }
+    }
+
+    const goBack = () => {
+        setBlockScroll(true);
+        router.back();
+        setTimeout(() => {
+            setBlockScroll(false);
+        }, 500)
     }
 
     return (<>
