7b7c861b7464145476089ff0897ebe00c7ac96f9
jwkaterina <jwkaterina@gmail.com>
2024-01-06 09:45:44 +0100

    remove unnesseccary validation

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controllers');
 
@@ -8,17 +7,7 @@ const router = express.Router();
 router.get('/', usersController.getUsers);
 
 router.post(
-  '/signup',
-  [
-    check('name')
-      .not()
-      .isEmpty(),
-    check('email')
-      .normalizeEmail() // Test@test.com => test@test.com
-      .isEmail(),
-    check('password').isLength({ min: 6 })
-  ],
-  usersController.signup
+  '/signup', usersController.signup
 );
 
 router.post('/login', usersController.login);
