b0ea5cd37d43032a5572a5c67e31e9aa1b4523c6
jwkaterina <jwkaterina@gmail.com>
2024-01-09 15:26:05 +0100

    remove unnesseccary code

--- src/app/analysis/menu-analysis/page.tsx
+++ src/app/analysis/menu-analysis/page.tsx
@@ -1,32 +1,13 @@
 'use client'
 
-// import styles from './page.module.css'
-// import { fetchNutritionAnalysisAPI } from '@/app/services/fetch-data'
-// import { useState } from 'react' 
 import NavBar from '@/app/components/navigation/nav-bar';
 import AnalysisMenu from '@/app/components/navigation/menus/analysis-menu';
 
 const MenuSearch = (): JSX.Element => {
 
-  // const [data, setData] = useState({calories: 0});
-
-  // useEffect(() => {
-  //   async function fetchData() {
-
-  //     const response = await fetchNutritionAnalysisAPI();
-  //     // console.log(response);
-  //     setData(response);
-  //   }
-  //   fetchData();
-  // }, []);
-
-  const handleHeaderClick = () => {
-    // console.log('header clicked');
-  }
-
   return (<>
     <NavBar color={'var(--secondary-color)'}>
-        <AnalysisMenu title="Menu" onHeaderClick={handleHeaderClick}/>
+        <AnalysisMenu title="Menu"/>
     </NavBar>
       <h1>Menu Search</h1>
   </>
--- src/app/analysis/recipe-analysis/page.tsx
+++ src/app/analysis/recipe-analysis/page.tsx
@@ -1,31 +1,13 @@
 'use client'
 
-// import styles from './page.module.css'
-// import { fetchNutritionAnalysisAPI } from '@/app/services/fetch-data'
 import NavBar from '@/app/components/navigation/nav-bar';
 import AnalysisMenu from '@/app/components/navigation/menus/analysis-menu';
 
 const RecipeSearch = (): JSX.Element => {
 
-  // const [data, setData] = useState({calories: 0});
-
-  // useEffect(() => {
-  //   async function fetchData() {
-
-  //     const response = await fetchNutritionAnalysisAPI();
-  //     console.log(response);
-  //     setData(response);
-  //   }
-  //   fetchData();
-  // }, []);
-
-  const handleHeaderClick = () => {
-    // console.log('header clicked');
-  }
-
   return (<>
    	<NavBar color={'var(--secondary-color)'}>
-        <AnalysisMenu title="Food" onHeaderClick={handleHeaderClick}/>
+        <AnalysisMenu title="Food"/>
     </NavBar>
     <h1>Recipe Search</h1>
     </>  )
