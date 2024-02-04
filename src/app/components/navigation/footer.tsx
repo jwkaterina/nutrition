5b29f8a820e31e1dde99554299290e6ff6b61cd6
jwkaterina <jwkaterina@gmail.com>
2024-02-04 12:06:39 +0100

    add home button to footer

--- src/app/components/navigation/footer.tsx
+++ src/app/components/navigation/footer.tsx
@@ -2,6 +2,11 @@ import styles from "./nav-bar.module.css"
 import { useRouter } from 'next/navigation'
 import { useContext } from "react";
 import { AuthContext } from "@/app/context/auth-context";
+import { SlideContext } from "@/app/context/slide-context";
+import { CardState } from "@/app/types/types";
+import { CardOpenContext } from "@/app/context/card-context";
+import { CurrentRecipeContext } from "@/app/context/recipe-context";
+import { CurrentMenuContext } from "@/app/context/menu-context";
 
 interface FooterProps {
     color: string,
@@ -11,6 +16,10 @@ const Footer = ({ color }: FooterProps): JSX.Element => {
 
     const router = useRouter();
     const { isLoggedIn } = useContext(AuthContext);
+    const { setCardOpen } = useContext(CardOpenContext);
+    const { setScrollBehavior } = useContext(SlideContext);
+    const { setCurrentRecipe } = useContext(CurrentRecipeContext);
+    const { setCurrentMenu } = useContext(CurrentMenuContext);
 
     const handleAuthClick = () => {
         if (isLoggedIn) {
@@ -20,10 +29,21 @@ const Footer = ({ color }: FooterProps): JSX.Element => {
         }
     }
 
+    const handleSettingsClick = () => {
+        setCardOpen(CardState.CLOSED);
+        setCurrentRecipe({id: null, recipe: null});
+		setCurrentMenu({id: null, menu: null});
+        setScrollBehavior('auto');
+        router.push('/');
+        setTimeout(() => {
+            setScrollBehavior('smooth');
+        }, 500);
+    }
+
     return (
         <nav className={styles.footer} style={{background: color}}>
             <div className={styles.footer_links}>
-				<a className={styles.link} onClick={() => console.log('settings')}>Settings</a>
+				<a className={styles.link} onClick={handleSettingsClick}>Home</a>
 				<a className={styles.link} onClick={handleAuthClick}>{isLoggedIn ? 'Logout' : 'Login'}</a>
 		    </div>
         </nav>
