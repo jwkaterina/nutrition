081c56eda2624d7e14ce4521cfd2661549b2420b
jwkaterina <jwkaterina@gmail.com>
2024-03-08 13:47:03 +0100

    comment out amplifyconfiguration path

--- src/app/components/amplify/configure_amplify.tsx
+++ src/app/components/amplify/configure_amplify.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import config from "@/app/amplifyconfiguration.json";
+// import config from "@/app/amplifyconfiguration.json";
 import { Amplify } from "aws-amplify";
 
-Amplify.configure(config, { ssr: true });
+// Amplify.configure(config, { ssr: true });
 
 export default function ConfigureAmplifyClientSide() {
   return null;
