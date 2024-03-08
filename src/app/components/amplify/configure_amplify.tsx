ee6b19d9e988163e2c6512d4fe6bafbe62866717
jwkaterina <jwkaterina@gmail.com>
2024-03-08 13:39:45 +0100

    update amplifyconfiguration import

--- src/app/components/amplify/configure_amplify.tsx
+++ src/app/components/amplify/configure_amplify.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import config from "@/amplifyconfiguration.json";
+import config from "@/app/amplifyconfiguration.json";
 import { Amplify } from "aws-amplify";
 
 Amplify.configure(config, { ssr: true });
