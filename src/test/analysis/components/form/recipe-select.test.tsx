0c479cb92f3dc80b8641bec84336109aa3baa4d0
jwkaterina <jwkaterina@gmail.com>
2024-04-02 21:23:11 +0200

    test recipe select

---
+++ src/test/analysis/components/form/recipe-select.test.tsx
@@ -0,0 +1,61 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import RecipeSelect from '../../../../app/analysis/components/form/recipe-select';
+import loadedRecipe from '../../../objects/loaded-recipe.json';
+
+describe('recipe-select', () => {
+
+    it('should render recipe-select', async() => {
+        const props = {
+            inputs: 1,
+            setCurrentRecipes: jest.fn(),
+            currentRecipes: [{
+                selectedRecipe: loadedRecipe.recipe,
+                selectedServings: 2
+            }],
+            loadedRecipes: [loadedRecipe]
+        }
+        
+        render(<RecipeSelect {...props} />);
+   
+        const numberInput = screen.getByRole('spinbutton', {
+            name: /servings/i
+        });
+
+        await user.type(numberInput, '5');
+        expect(props.setCurrentRecipes).toHaveBeenCalledWith([{
+            selectedRecipe: loadedRecipe.recipe,
+            selectedServings: 25
+        }]);
+
+        screen.logTestingPlaygroundURL();
+
+    });
+
+    it('should render recipe-select', async() => {
+        const props = {
+            inputs: 1,
+            setCurrentRecipes: jest.fn(),
+            currentRecipes: [{
+                selectedRecipe: loadedRecipe.recipe,
+                selectedServings: 2
+            }],
+            loadedRecipes: [loadedRecipe]
+        }
+        
+        render(<RecipeSelect {...props} />);
+
+
+        const selectInput = screen.getByRole('combobox');
+
+        await user.selectOptions(selectInput, 'Recipe 40');
+        expect(props.setCurrentRecipes).toHaveBeenCalledWith([{
+            selectedRecipe: loadedRecipe.recipe,
+            selectedServings: 2
+        }]);
+
+        screen.logTestingPlaygroundURL();
+
+    });
+});
