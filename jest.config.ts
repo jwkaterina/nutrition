a96341612802a080f3719b18b3c73e467983a19a
jwkaterina <jwkaterina@gmail.com>
2024-04-26 18:24:38 +0200

    Generate coverage-summary.json

--- jest.config.ts
+++ jest.config.ts
@@ -42,12 +42,10 @@ const config: Config = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "json-summary",
+    "lcov",
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
